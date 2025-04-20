352fbcbc36e12f50676865d83ba8dccc4df0a76b
ErickMateus-Silva <erickmateusls0510@gmail.com>
2025-04-20 06:24:42 -0300

    feat: Cria Repository e Service Ninja e Missoes + adicionar @Column

---
+++ src/main/java/dev/Java10x/CadastroDeNinjas/Ninjas/Controller/Service/NinjaRepository.java
@@ -0,0 +1,7 @@
+package dev.Java10x.CadastroDeNinjas.Ninjas.Controller.Service;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NinjaRepository extends JpaRepository<NinjaModel, Long> {
+
+}
